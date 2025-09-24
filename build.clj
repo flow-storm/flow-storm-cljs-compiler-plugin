ea16b1e2e4df018a801a1591054eaea297e512b0
Juan Monetta <jpmonettas@gmail.com>
2025-09-24 10:09:01 -0300

    Release 1.0.0-beta5

--- build.clj
+++ build.clj
@@ -5,7 +5,7 @@
             [clojure.spec.alpha :as s]))
 
 (def version (or (System/getenv "VERSION")
-                 "1.0.0-beta4"))
+                 "1.0.0-beta5"))
 
 (def target-dir "target")
 (def class-dir (str target-dir "/classes"))
